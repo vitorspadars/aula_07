759ea225cdb88ec8a0611c98856a86ae569e7560
vitorspada <vitorspada.rs@gmail.com>
2026-07-09 10:03:37 -0300

    exercicio 2

--- exercicios.py
+++ exercicios.py
@@ -1,14 +1,31 @@
-# 1. Calcular Média de Valores em uma Lista
-
 from typing import List
 
 valor: List = [5.0,7.0,8.0,10.0]
-"""
-Receba uma lista e retorna a média dela em float
-"""
-def media_de_lista(valores: List[float]) -> float:
-    return sum(valores) / len(valores)
+# 1. Calcular Média de Valores em uma Lista
+# """
+# Receba uma lista e retorna a média dela em float
+# """
+# def media_de_lista(valores: List[float]) -> float:
+#     return sum(valores) / len(valores)
 
-resultado = media_de_lista(valor)
+# resultado = media_de_lista(valor)
 
-print(resultado)
+# print(resultado)
+
+
+# Filtrar Dados Acima de um Limite
+
+def filtrar_valores(limite: float, lista: List[float]):
+    """
+    Recebe um valor de corte e uma lista de valores, 
+    retorna uma lista de valores maior do que o valor de corte
+    """
+    valores_filtrados: List[float] = []
+    for i in lista:
+        if i > limite:
+            valores_filtrados.append(i)
+    return valores_filtrados
+
+resultados = filtrar_valores(7, valor)
+
+print(resultados)
