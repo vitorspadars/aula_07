16b7961308a2d6b1332e9c582a532ecfdcfa0153
vitorspada <vitorspada.rs@gmail.com>
2026-07-09 09:31:26 -0300

    fazendo o primeiro exercicio de função

---
+++ exemplo_soma.py
@@ -0,0 +1,7 @@
+def soma(valor_1_para_soma: float, valor_2_para_soma: float) -> float:
+    """
+    Uma função simples para a somar, recebe valores float e retorna valores float
+    """
+    resultado_da_soma: float = valor_1_para_soma + valor_2_para_soma
+
+    return resultado_da_soma
