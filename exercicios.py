8be37ff01c05d80d771cf9fc440234c63809029e
vitorspada <vitorspada.rs@gmail.com>
2026-07-09 10:27:38 -0300

    realização dos exercicio propostos

--- exercicios.py
+++ exercicios.py
@@ -1,19 +1,17 @@
 from typing import List
 
-valor: List = [5.0,7.0,8.0,10.0]
+valor: List = [5, 5, 7, 8, 8, 8, 10, 15]
 # 1. Calcular Média de Valores em uma Lista
-# """
-# Receba uma lista e retorna a média dela em float
-# """
-# def media_de_lista(valores: List[float]) -> float:
-#     return sum(valores) / len(valores)
 
-# resultado = media_de_lista(valor)
-
-# print(resultado)
+def media_de_lista(valores: List[float]) -> float:
+    """
+    Receba uma lista e retorna a média dela em float
+    """
+    return sum(valores) / len(valores)
 
 
-# Filtrar Dados Acima de um Limite
+
+# 2. Filtrar Dados Acima de um Limite
 
 def filtrar_valores(limite: float, lista: List[float]):
     """
@@ -29,3 +27,31 @@ def filtrar_valores(limite: float, lista: List[float]):
 resultados = filtrar_valores(7, valor)
 
 print(resultados)
+
+# 3. Contar Valores Únicos em uma Lista
+
+def contador_de_valores_unicos(lista: List[float]) -> int:
+
+    valores_unicos = len(set(lista))
+    return valores_unicos
+
+
+# 4. Converter Celsius para Fahrenheit em uma Lista
+
+def conversor_celsius_fahrenheit(temperaturas_celsius: List[float]) -> List[float]:
+    fahrenheit = [(9/5) * temp + 32 for temp in temperaturas_celsius]
+    return fahrenheit
+
+# 5. Calcular Desvio Padrão de uma Lista
+
+def desvio_padrao_lista(lista: List[float]) -> float:
+    media_valores: float = sum(lista) / len(lista)
+    variancia_valores: float = sum((x - media_valores) ** 2 for x in lista) / len(lista)
+    resultado: float = variancia_valores ** 0.5
+    return round(resultado, 2)
+
+# 6. Encontrar Valores Ausentes em uma Sequência
+
+def encontrar_valores_ausentes(sequencia: List[int]) -> List[int]:
+    completo = set(range(min(sequencia), max(sequencia) + 1))
+    return list(completo - set(sequencia))
